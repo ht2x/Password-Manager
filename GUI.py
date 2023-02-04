ead7fc95555e1952eb21afdca8543ce21c34185a
ht2x <96992532+ht2x@users.noreply.github.com>
2023-02-04 19:13:02 +0300

    First iteration

--- GUI.py
+++ GUI.py
@@ -1,14 +1,32 @@
 from tkinter import ttk
 from tkinter import *
+from PIL import ImageTk, Image
 
 window = Tk()
-window.geometry("900x500")
+window.geometry("600x400")
+window.title("Youtube Downloader")
+window.configure(background='#252525')
 
-frame = ttk.Frame(window, padding=20)
-frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+s = ttk.Style()
+s.configure('TFrame', background='#252525')
 
-ttk.Label(frame, text="Hello World!")                   .grid(column=0, row=0)
-ttk.Button(frame, text="Quit", command=window.destroy)  .grid(column=1, row=0)
+frame = ttk.Frame(window,padding=5)
+frame.grid(row=0, column=0)
+frame.place(relx=.5, rely=0.5, anchor=CENTER)
 
-window.minsize(900,500)
+imge = Image.open("Youtube-logo-vector-PNG.png")
+ytlogo = imge.resize((300,300))
+img = ImageTk.PhotoImage(ytlogo)
+
+label = Label(frame, image = img, background='#252525').grid(column=0, row=0, columnspan=3, pady=0, padx=50)
+
+field = ttk.Entry(frame)                   
+field.grid(column=0, row=1, columnspan=3, sticky="nsew", ipady=5)
+
+ttk.Button(frame, text="New Download", command=lambda:field.delete(0,'end'), padding=4).grid(column=0, row=2, sticky="W", pady=5)
+ttk.Button(frame, text="360p", command=lambda:field.delete(0,'end'), padding=4).grid(column=1, row=2, sticky="E", pady=5)
+ttk.Button(frame, text="720p", command=lambda:field.delete(0,'end'), padding=4).grid(column=2, row=2, sticky="WE", pady=5)
+
+window.minsize(600,400)
+window.maxsize(600,400)
 window.mainloop()
