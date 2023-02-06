9df74260901e5a46430499da86f3727ec0025824
ht2x <96992532+ht2x@users.noreply.github.com>
2023-02-06 15:08:00 +0300

    added comments

--- Downloader.py
+++ Downloader.py
@@ -1,42 +1,45 @@
-from pytube import YouTube 
-import os
+from pytube import YouTube #import the YouTube object from the pytube library
+import os #import the system library
 
-videos_path = f"C:/Users/{os.getlogin()}/Videos"
+videos_path = f"C:/Users/{os.getlogin()}/Videos" #decide the video download path
 
-class StreamsMetaData:
-    code = None
-    videos = None
-    quality = 360
+class StreamsMetaData: # create a metadata object
+    code = None # error/success code
+    videos = None # list of streams, streams being the same video in different resolutions 
+    quality = 360 # desired resolution
 
-    def __init__(self, code, videos = None, quality = None):
-        self.code = code
-        self.videos = videos
-        self.quality = quality    
+    def __init__(self, code, videos = None, quality = None): # initialization function, taking same arguments as above variables
+        self.code = code        #assignment
+        self.videos = videos    #//
+        self.quality = quality  #//
 
-def InitailizeDownload(link:str, quality:int) -> StreamsMetaData:
-    try:
-        video_data = YouTube(link)
-    except:
-        if len(link) == 0:
-            return StreamsMetaData(401)
-        elif "youtube.com" not in link:
-            return StreamsMetaData(402)
+def InitailizeDownload(link:str, quality:int) -> StreamsMetaData: # a function to start the download process, returns an object of type StreamsMetaData
+    try: #error handling
+        video_data = YouTube(link) #get the video data using the link
+    except: # in case of an error
+        if len(link) == 0: #if the field is empty
+            return StreamsMetaData(401) # return only code, which is an erro
+        elif "youtube.com" not in link: #if the link isn't from youtube
+            return StreamsMetaData(402) #//
         else:
-            return StreamsMetaData(400)
+            return StreamsMetaData(400) #//
 
-    try:
-        mp4_videos = video_data.streams.filter(mime_type="video/mp4", progressive="True")
-        return StreamsMetaData(200, mp4_videos, quality)
-    except:
-        return StreamsMetaData(502)
+    try: #error handling
+        mp4_videos = video_data.streams.filter(mime_type="video/mp4", progressive="True") #filter unwanted data and assign the list of videos(streams) to 'mp4_videos'
+        return StreamsMetaData(200, mp4_videos, quality) #return code 200 which indicate success and return other necessary data
+    except: # in case of an error
+        return StreamsMetaData(502) # return only code, which is an erro
 
-    return StreamsMetaData(500)
+    return StreamsMetaData(500) # return only code, which is an erro
             
-def DownloadInProgress(mp4_videos, quality):
-    for video in mp4_videos:
-        try:
-            if int(video.resolution.replace("p", "")) >= quality:
-                video.download(output_path=videos_path)
-                return 200
-        except:
-            return 503
+def DownloadInProgress(mp4_videos, quality): # the process of downloading the video itself
+    for video in mp4_videos: #loop over each stream to compare the quality 
+        try: #error handling
+            if int(video.resolution.replace("p", "")) >= quality: #compare the quality of the stream to the desired quality
+                video.download(output_path=videos_path) #if found, download the video
+                return 200 #return a success code
+        except: # in case of an error (usually network error)
+            return 503 #return a failure code
+
+    mp4_videos.first().download(output_path=videos_path) #if the desired quality wasn't found, download the first stream (usually worse quality)
+    return 200 #return a success code
--- GUI.py
+++ GUI.py
@@ -1,71 +1,54 @@
-from tkinter import ttk
-from tkinter import *
-from tkinter import messagebox
-from PIL import ImageTk, Image
-from Downloader import InitailizeDownload, DownloadInProgress
-import os; username = os.getlogin()
+from tkinter import ttk         #importing tkinter for GUI
+from tkinter import *           #//
+from tkinter import messagebox  #//
+from PIL import ImageTk, Image  #importing PILLOW for image editing
+from Downloader import InitailizeDownload, DownloadInProgress #importing our custom module to download youtube videos
+import os; username = os.getlogin() #import os library to get the username of the user
 
-window = Tk()
-window.geometry("600x400")
-window.title("Youtube Downloader")
-window.configure(background='#252525')
+window = Tk()   #initializing the window
+window.geometry("600x400") #setting the window size
+window.title("Youtube Downloader") #setting the window's title
+window.configure(background='#252525') #setting the window's background color to dark gray (#252525)
 
-s = ttk.Style()
-s.configure('TFrame', background='#252525')
-s.configure('First.TFrame', background='#FFFFFF')
-ttk.Style().configure("TButton", padding=4, background="#252525")
+s = ttk.Style() #initializing the style object to make every object's background color as dark gray
+s.configure('TFrame', background='#252525') #creating the master Frame style, so every frame have the same background by default
+s.configure('First.TFrame', background='#FFFFFF') #creating a child Frame style which makes the desired Frame White
+ttk.Style().configure("TButton", padding=4, background="#252525") #creating the master Button style
 
-frame = ttk.Frame(window,padding=5)
-frame.grid(row=0, column=0)
-frame.place(relx=.5, rely=0.5, anchor=CENTER)
+frame = ttk.Frame(window,padding=5) #creating the frame
+frame.grid(row=0, column=0) #assign the frame to the grid
+frame.place(relx=.5, rely=0.5, anchor=CENTER) #place the frame in the center
 
-imge = Image.open("Youtube-logo-vector-PNG.png")
-ytlogo = imge.resize((300,300))
-img = ImageTk.PhotoImage(ytlogo)
-label = Label(frame, image = img, background='#252525').grid(column=0, row=0, columnspan=3, pady=0, padx=50)
+original_image = Image.open("Youtube-logo-vector-PNG.png") #open the image file in the program : PIL
+ytlogo = original_image.resize((300,300)) #modify the image size to 300p x 300p and assign it to 'ytlogo'
+img = ImageTk.PhotoImage(ytlogo) #turn the 'ytlogo' object to be compatible with TKinter
+label = Label(frame, image = img, background='#252525').grid(column=0, row=0, columnspan=3, pady=0, padx=50) #create a label object and assign the image to it
 
-field = ttk.Entry(frame)
-field.grid(column=0, row=1, columnspan=3, sticky="nsew", ipady=5)
+field = ttk.Entry(frame) #making an entry field to allow the user to copy and paste the Link in
+field.grid(column=0, row=1, columnspan=3, sticky="nsew", ipady=5) #assing the field to the grid and making it span over 3 columns
 
-def InitializePopUp(metaData = None):
-    top = Toplevel(window)
-    top.title("success")
-    top.geometry("300x150")
-    top.minsize(300,150)
-    top.maxsize(300,150)
-    try:
-        if DownloadInProgress(metaData.videos, metaData.quality) == 200:
-            popUpFrame = ttk.Frame(top, style='First.TFrame')
-            popUpFrame.place(relx=.5, rely=0.5, anchor=CENTER)
-
-            field.delete(0,'end')
-
-            Label(popUpFrame, text=f"Your download is finished.\n You can find the file in: C:/Users/{username}/Videos").grid(column=0, row=0, columnspan=2)
-            ttk.Button(popUpFrame, text= "Close", command=top.destroy).grid(column=0, row=1, padx=5)
-            ttk.Button(popUpFrame, text= "Open File Location", command=lambda:os.system(f'start {os.path.realpath(f"C:/Users/{username}/Videos")}')).grid(column=1, row=1, padx=5)
-        else:
-            top.destroy()
-            messagebox.showerror("error", "There was an error while downloading the video.\n Please check your internet connection. (error code: 503)")
-    except:
-        top.destroy()
-        messagebox.showerror("error", "There was an error while downloading the video.\n Please check your internet connection. (error code: 511)")
-
-def EvaluateOutcome(metaData):
-    if metaData.code == 200:
-        InitializePopUp(metaData)
-    elif metaData.code == 400:
+def EvaluateOutcome(metaData): #function to allow error handling by a code 
+    if metaData.code == 200: # success
+        try:
+            if DownloadInProgress(metaData.videos, metaData.quality) == 200: #Download the video then evaluate the code
+                os.system(f'start {os.path.realpath(f"C:/Users/{username}/Videos")}') #open the file location
+            else:
+                messagebox.showerror("error", "There was an error while downloading the video.\n Please check your internet connection. (error code: 503)")
+        except:
+            messagebox.showerror("error", "There was an error while downloading the video.\n Please check your internet connection. (error code: 511)")
+    elif metaData.code == 400: # faliure
         messagebox.showerror("error", f"There was a connection error.\n(error code: {metaData.code})")
-    elif metaData.code == 401:
+    elif metaData.code == 401: # faliure
         messagebox.showerror("error", f"You cannot submit an empty link.\n (error code: {metaData.code})")
-    elif metaData.code == 402:
+    elif metaData.code == 402: # faliure
         messagebox.showerror("error", f"You can only download from 'youtube.com'.\n (error code: {metaData.code})")
-    else:
+    else: # faliure
         messagebox.showerror("error", f"there was an unknown error, Please contact developer.\n (error code: {metaData.code})")
 
-ttk.Button(frame, text="New Download", command=lambda:field.delete(0,'end')).grid(column=0, row=2, sticky="W", pady=5)
-ttk.Button(frame, text="360p", command=lambda:EvaluateOutcome(InitailizeDownload(field.get(), 360))).grid(column=1, row=2, sticky="E", pady=5)
-ttk.Button(frame, text="720p", command=lambda:EvaluateOutcome(InitailizeDownload(field.get(), 720))).grid(column=2, row=2, sticky="WE", pady=5)
+ttk.Button(frame, text="New Download", command=lambda:field.delete(0,'end')).grid(column=0, row=2, sticky="W", pady=5) #make a button
+ttk.Button(frame, text="360p", command=lambda:EvaluateOutcome(InitailizeDownload(field.get(), 360))).grid(column=1, row=2, sticky="E", pady=5) #make a button
+ttk.Button(frame, text="720p", command=lambda:EvaluateOutcome(InitailizeDownload(field.get(), 720))).grid(column=2, row=2, sticky="WE", pady=5) #make a button
 
-window.minsize(600,400)
-window.maxsize(600,400)
-window.mainloop()
+window.minsize(600,400) #set the minimum size of the window
+window.maxsize(600,400) #set the maximum size of the window
+window.mainloop() # START THE PROGRAM
